c0f816d95bcdab3ee3d94eafb3b6c79a70c23fce
Ayodeji <ayodwjimarvelous87@gmail.com>
2026-07-23 07:50:54 +0100

    Use same backend for login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -19,7 +19,7 @@ const [loading,setLoading] = useState(false);
       setMessage("");
 
       const res = await fetch(
-        "https://alphabot-i7p2.onrender.com/users/login",
+        "https://alphabot-1.onrender.com/users/login",
         {
           method:"POST",
           headers:{
